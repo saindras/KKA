938646a8fb0a55b42e479cf62f76fc2768903c91
Gede Saindra Santyadiputra <gsaindras@undiksha.ac.id>
2025-10-23 19:58:31 +0800

    Change 'torch_dtype' to 'dtype' in pipeline

--- Aplikasi/VSCode/pirate-chatbot-main-tkinter.py
+++ Aplikasi/VSCode/pirate-chatbot-main-tkinter.py
@@ -9,7 +9,7 @@ model_id = "meta-llama/Llama-3.2-3B-Instruct"
 pipe = pipeline(
     "text-generation",
     model=model_id,
-    torch_dtype=torch.bfloat16,
+    dtype=torch.bfloat16,
     device_map="auto",
 )
 
